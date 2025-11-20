e3484d6199f58439000c6b1f976f621addc95d2e
Lauri Koskela <luryus@users.noreply.github.com>
2025-11-20 20:09:36 +0200

    Initial commit: Fix proto syntax and update Program.cs

---
+++ TestProgram/Program.cs
@@ -0,0 +1,15 @@
+﻿using TestProgram.Protos;
+
+var parent = new Parent();
+
+// This line should produce a warning because Child can be null, 
+// but the generated code likely doesn't annotate it as nullable.
+Console.WriteLine(parent.Child.Name);
+
+// Similarly for the string wrapper
+if (parent.Child != null)
+{
+    // Nickname is a StringValue, which is a message, so it can be null.
+    // But generated property probably isn't nullable.
+    Console.WriteLine(parent.Child.Nickname);
+}
